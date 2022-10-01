54351301fdf49901277e3c437ebfa9689d92f09d
PhamHuyHiep45200 <huyhiep4520@gmail>
2022-10-01 14:34:27 +0700

    layout

---
+++ src/data/MenuLauout.ts
@@ -0,0 +1,29 @@
+
+export interface Menu{
+    id:number,
+    name:string,
+    router:string
+}
+
+export const MenuLayoutTop:Menu[]=[
+    {
+        id:1,
+        name:'Trang chủ',
+        router:'/'
+    },
+    {
+        id:2,
+        name:'Trang chủ',
+        router:'/'
+    },
+    {
+        id:3,
+        name:'Trang chủ',
+        router:'/'
+    },
+    {
+        id:4,
+        name:'Trang chủ',
+        router:'/'
+    }
+]
--- src/layout/layoutTop/index.tsx
+++ src/layout/layoutTop/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Space, Row, Col } from "antd";
+import { Menu, MenuLayoutTop } from "../../data/MenuLauout";
+import { Link } from "react-router-dom";
+import styles from "../../styles/layout.module.scss";
 
 function LayoutTop() {
-  return <div>ok</div>;
+  return (
+    <Row justify="center" className={styles.layout}>
+      <Col span={22}>
+        <Row>
+          <Col span={4}>logo</Col>
+          <Col span={16}>
+            <Space style={{ width: "100%", justifyContent: "space-around" }}>
+              {MenuLayoutTop.map((menu: Menu) => (
+                <Link to={menu.router} key={menu.id}>
+                  {menu.name}
+                </Link>
+              ))}
+            </Space>
+          </Col>
+          <Col span={4} style={{ textAlign: "right" }}>
+            login
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
 }
 
 export default LayoutTop;
